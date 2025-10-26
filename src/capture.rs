c420629b40e9d041f2cc8830e1c7b9defcfb73f9
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2025-10-27 01:48:01 +0800

    Introduce BackendType and move existing impl to PktmonBackend

--- src/capture.rs
+++ src/capture.rs
@@ -1,10 +1,10 @@
+#[cfg(windows)]
+mod pktmon_backend;
+
 use std::fmt::{Debug, Display};
 
 use anyhow::Error;
-use futures::StreamExt;
-use futures::stream::FusedStream;
-use pktmon::filter::{PktMonFilter, TransportProtocol};
-use pktmon::{Capture, Packet};
+use async_trait::async_trait;
 
 pub const PORT_RANGE: (u16, u16) = (22101, 22102);
 
@@ -37,50 +37,34 @@ impl Display for CaptureError {
 
 pub type Result<T> = std::result::Result<T, CaptureError>;
 
-pub struct PacketCapture {
-    stream: Box<dyn FusedStream<Item = Packet> + Unpin + Send>,
+#[async_trait]
+pub trait CaptureBackend: Send {
+    async fn next_packet(&mut self) -> Result<Vec<u8>>;
 }
 
-impl PacketCapture {
-    pub fn new() -> Result<Self> {
-        let mut capture = Capture::new().map_err(|e| CaptureError::Capture {
+#[derive(Debug)]
+pub enum BackendType {
+    Pktmon,
+    Pcap,
+}
+
+#[cfg(windows)]
+pub const DEFAULT_CAPTURE_BACKEND_TYPE: BackendType = BackendType::Pktmon;
+#[cfg(not(windows))]
+pub const DEFAULT_CAPTURE_BACKEND_TYPE: BackendType = BackendType::Pcap;
+
+pub fn create_capture(backend: BackendType) -> Result<Box<dyn CaptureBackend>> {
+    match backend {
+        #[cfg(windows)]
+        BackendType::Pktmon => Ok(Box::new(pktmon_backend::PktmonBackend::new()?)),
+        BackendType::Pcap => todo!(),
+        #[allow(unreachable_patterns)]
+        _ => Err(CaptureError::Capture {
             has_captured: false,
-            error: e.into(),
-        })?;
-
-        let filter = PktMonFilter {
-            name: "UDP Filter".to_string(),
-            transport_protocol: Some(TransportProtocol::UDP),
-            port: PORT_RANGE.0.into(),
-            ..PktMonFilter::default()
-        };
-
-        capture
-            .add_filter(filter)
-            .map_err(|e| CaptureError::Filter(e.into()))?;
-
-        let filter = PktMonFilter {
-            name: "UDP Filter".to_string(),
-            transport_protocol: Some(TransportProtocol::UDP),
-            port: PORT_RANGE.1.into(),
-            ..PktMonFilter::default()
-        };
-
-        capture
-            .add_filter(filter)
-            .map_err(|e| CaptureError::Filter(e.into()))?;
-
-        Ok(Self {
-            stream: Box::new(capture.stream().unwrap().boxed().fuse()),
-        })
-    }
-
-    pub async fn next_packet(&mut self) -> Result<Vec<u8>> {
-        futures::select! {
-            packet = self.stream.select_next_some() => {
-                Ok(packet.payload.to_vec().clone())
-            },
-            complete => Err(CaptureError::CaptureClosed),
-        }
+            error: anyhow::anyhow!(
+                "Capture backend type {:?} not supported on this operating system",
+                backend
+            ),
+        }),
     }
 }
