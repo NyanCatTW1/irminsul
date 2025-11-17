4420394bdd289e2981638c05755f7d82d41a173c
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-17 13:53:00 +1100

    build: :construction_worker: use pre-release identifier in app & github releases

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use std::{env, io};
 
+use git_version::git_version;
 use winresource::WindowsResource;
 
 fn main() -> io::Result<()> {
@@ -13,5 +14,21 @@ fn main() -> io::Result<()> {
     {
         println!("cargo:rustc-link-lib=static=pcap");
     }
+
+    // Set version with git hash for pre-releases
+    let version = env!("CARGO_PKG_VERSION");
+    let is_release = env::var("RELEASE_BUILD").is_ok();
+
+    let full_version = if is_release {
+        version.to_string()
+    } else {
+        let git_hash = git_version!(args = ["--short", "--always"], fallback = "unknown");
+        format!("{}-pre.{}", version, git_hash)
+    };
+
+    println!("cargo:rustc-env=APP_VERSION={}", full_version);
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-env-changed=RELEASE_BUILD");
+
     Ok(())
 }
